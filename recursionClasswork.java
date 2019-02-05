b907f9202b82bd787e7e9a79d3250f12635b70c3
TiffanyCao <tcao00@stuy.edu>
2019-02-04 19:19:55 -0800

    one successful test case

--- recursionClasswork.java
+++ recursionClasswork.java
@@ -1,26 +1,28 @@
 import java.util.*;
 
-public class Words{
+public class recursionClasswork{
   public static List<String> makeAllWords(int k, int maxLetter){
     List<String> allWords = new ArrayList<String>();
-    String temp = "";
-    makeWordsH(k, temp, maxLetter, allWords);
+    makeWordsH(k, "", maxLetter, allWords);
     return allWords;
   }
 
   private static void makeWordsH(int k, String temp, int maxLetter, List<String> data){
     if(k == 0){
       data.add(temp);
+      temp = "";
     }else{
-      makeWordsH(k-1, temp+=(char)97, maxLetter, data);
-      makeWordsH(k-1, temp+=(char)98, maxLetter, data);
-      //makeWordsH(k-1, temp+=(char)99, maxLetter, data);
+      String t = temp;
+      for(int i = 1; i <= maxLetter; i++){
+        makeWordsH(k-1, temp+=(char)(96+i), maxLetter, data);
+        temp = t;
+      }
     }
   }
 
   public static void main(String args[]){
     //System.out.println(makeAllWords(1, 26));
     System.out.println(makeAllWords(2, 2));
-    //System.out.println(makeAllWords(2, 2));
+    //System.out.println(makeAllWords(2, 3));
   }
 }
